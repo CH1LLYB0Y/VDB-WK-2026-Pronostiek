9c2d09a3e561731252970ba0d2c352c2d154a802
CH1LLYB0Y <sil.vandengroenendal@gmail.com>
2025-11-24 16:57:59 +0100

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Aptos', 'sans-serif'], // Zet Aptos als standaard sans font
+      },
+      colors: {
+        wkGreen: '#3CAC3B',
+        wkBlue: '#2A398D',
+        wkRed: '#E61D25',
+        wkLightGray: '#D1D4D1',
+        wkDarkGray: '#474A4A',
+      },
+    },
   },
   plugins: [],
 }
