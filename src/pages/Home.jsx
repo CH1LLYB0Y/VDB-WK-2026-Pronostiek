c88afbafd5a187ffd8770d69caa8e230d9f44927
CH1LLYB0Y <sil.vandengroenendal@gmail.com>
2025-11-24 17:09:48 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,23 +5,19 @@ export default function Home() {
     <div className="home-container">
 
       {/* HEADER */}
-      <header className="home-header">
-        <img 
-          src="/logo.png" 
-          alt="WK 2026" 
-          className="home-logo"
-        />
+      <div className="home-header">
+        <img src="/logo.png" alt="WK 2026" className="home-logo" />
         <h1 className="home-title">VDB WK 2026 Pronostiek</h1>
-      </header>
+      </div>
 
       {/* BANNER GRID */}
       <div className="banner-grid">
 
-        <Link to="/pronostiek" className="banner-tile banner-green">
+        <Link to="/pronostiek" className="banner-tile banner-green banner-pronostiek">
           <h2>Pronostiek invullen</h2>
         </Link>
 
-        <Link to="/klassement" className="banner-tile banner-blue">
+        <Link to="/klassement" className="banner-tile banner-blue banner-klassement">
           <h2>Bekijk Klassement</h2>
         </Link>
 
